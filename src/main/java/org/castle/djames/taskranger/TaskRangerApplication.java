f5a50a95bd36cfebf16a1853578b786ae2855b48
DJames <davejames1109@gmail.com>
2023-01-30 21:27:10 +0800

    Initial Commit

---
+++ src/main/java/org/castle/djames/taskranger/TaskRangerApplication.java
@@ -0,0 +1,13 @@
+package org.castle.djames.taskranger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskRangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskRangerApplication.class, args);
+    }
+
+}
