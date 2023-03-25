d1371ef7b074ba495eb34871e8158bbb8caa2bf8
DJames <davejames1109@gmail.com>
2023-03-25 14:30:10 +0800

    Move build gradle of subprojects

--- broadcast-service/src/main/java/org/castle/djames/taskranger/broadcastservice/Application.java
+++ broadcast-service/src/main/java/org/castle/djames/taskranger/broadcastservice/Application.java
@@ -1,9 +1,9 @@
-package org.castle.djames.taskranger.commandservice;
+package org.castle.djames.taskranger.broadcastservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "org.castle.djames.taskranger.commandservice")
+@SpringBootApplication(scanBasePackages = "org.castle.djames.taskranger.broadcastservice")
 public class Application {
 
     public static void main(String[] args) {
--- query-service/src/main/java/org/castle/djames/taskranger/queryservice/Application.java
+++ query-service/src/main/java/org/castle/djames/taskranger/queryservice/Application.java
@@ -1,9 +1,9 @@
-package org.castle.djames.taskranger.commandservice;
+package org.castle.djames.taskranger.queryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "org.castle.djames.taskranger.commandservice")
+@SpringBootApplication(scanBasePackages = "org.castle.djames.taskranger.queryservice")
 public class Application {
 
     public static void main(String[] args) {
