fc8743bd92a458466e0f76806836a3d6d1f20b62
DJames <davejames1109@gmail.com>
2023-06-23 23:56:35 +0800

    Add UserController

---
+++ user-service/src/main/java/org/castle/djames/taskranger/userservice/controller/UserController.java
@@ -0,0 +1,10 @@
+package org.castle.djames.taskranger.userservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value = "/v1")
+@RestController
+public class UserController {
+
+}
